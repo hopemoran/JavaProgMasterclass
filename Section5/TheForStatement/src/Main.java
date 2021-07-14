aa6d7639629f06ccdbda6361002977a6845c3341
Hope Moran <hope.moran@bestbuy.com>
2021-07-14 13:00:40 -0500

    Section 5: Exercises Coding 13 - 66

--- Section5/TheForStatement/src/Main.java
+++ Section5/TheForStatement/src/Main.java
@@ -7,9 +7,35 @@ public class Main {
         System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
 
         for (int i = 2; i < 9; i++) {
-            double d = i;
-            System.out.println("10,000 at "  + d + "% interest = " + calculateInterest(10000.0, d));
+            System.out.println("10,000 at "  + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
+
+        for (int i = 8; i > 1; i--) {
+            System.out.println("10,000 at "  + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("Prime number: " + i);
+                count++;
+                if (count == 10) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n% i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
