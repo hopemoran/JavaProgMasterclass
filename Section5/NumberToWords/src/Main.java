a67bea07a2df4ffc3b7986d6df66c2753ef9b5d0
Hope Moran <hope.moran@bestbuy.com>
2021-07-19 11:36:56 -0500

    Section 5: Coding Ex 23

--- Section5/NumberToWords/src/Main.java
+++ Section5/NumberToWords/src/Main.java
@@ -2,10 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //getDigitCount() tests
         NumberToWords.getDigitCount(0);
         NumberToWords.getDigitCount(123);
         NumberToWords.getDigitCount(-12);
         NumberToWords.getDigitCount(5200);
+
+        //reverse() tests
+        NumberToWords.reverse(-121);
+        NumberToWords.reverse(1212);
+        NumberToWords.reverse(1234);
+        NumberToWords.reverse(100);
+
+
+        //numberToWords() tests
+        NumberToWords.numberToWords(123);
+        System.out.println("Next process\n");
+        NumberToWords.numberToWords(1010);
+        System.out.println("Next process\n");
+        NumberToWords.numberToWords(1000);
+        System.out.println("Next process\n");
+        NumberToWords.numberToWords(-12);
 
     }
 
--- Section5/NumberToWords/src/NumberToWords.java
+++ Section5/NumberToWords/src/NumberToWords.java
@@ -3,7 +3,7 @@ public class NumberToWords {
     public static int getDigitCount(int number) {
 
         if (number < 0) {
-            System.out.println("Invalid input");
+            System.out.println("Invalid Value");
             return -1;
         }
 
@@ -11,14 +11,12 @@ public class NumberToWords {
             int counter = 0;
             while (number >= 0) {
                 if (number == 0 && counter == 0) {
-                    System.out.println(++counter);
+                    ++counter;
                     return counter;
                 }
                 else if (number == 0 && counter != 0) {
-                    System.out.println(counter);
                     return counter;
                 }
-                System.out.println("number: " + number);
                 number = number / 10;
                 counter++;
             }
@@ -26,4 +24,68 @@ public class NumberToWords {
         return -1;
     }
 
+    public static int reverse(int number) {
+
+        int lastDigit;
+        int reverse = 0;
+
+        while (number != 0) {
+            lastDigit = number % 10;
+            number = number / 10;
+            reverse = reverse * 10;
+            reverse += lastDigit;
+        }
+
+        return reverse;
+    }
+
+    public static void numberToWords(int number) {
+
+        int digitCount = getDigitCount(number);
+        int numberReversed = reverse(number);
+        int lastDigit;
+        String numberToWordsString = "";
+
+        while (digitCount > 0) {
+            lastDigit = numberReversed % 10;
+            numberReversed = numberReversed / 10;
+
+            switch (lastDigit) {
+                case 0:
+                    numberToWordsString += "Zero\n";
+                    break;
+                case 1:
+                    numberToWordsString += "One\n";
+                    break;
+                case 2:
+                    numberToWordsString += "Two\n";
+                    break;
+                case 3:
+                    numberToWordsString += "Three\n";
+                    break;
+                case 4:
+                    numberToWordsString += "Four\n";
+                    break;
+                case 5:
+                    numberToWordsString += "Five\n";
+                    break;
+                case 6:
+                    numberToWordsString += "Six\n";
+                    break;
+                case 7:
+                    numberToWordsString += "Seven\n";
+                    break;
+                case 8:
+                    numberToWordsString += "Eight\n";
+                    break;
+                case 9:
+                    numberToWordsString += "Nine\n";
+                    break;
+            }
+
+            digitCount--;
+
+        }
+        System.out.println(numberToWordsString);
+    }
 }
