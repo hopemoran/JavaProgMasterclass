bcfdd2f7ca6879a8ac37182f53077a2866b1e846
Hope Moran <hope.moran@bestbuy.com>
2021-08-16 16:18:17 -0500

    Section 7: Ex. 93 - 94 & Coding Ex. 37 - 38

---
+++ Section7/Encapsulation/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        Player player = new Player();
+//        player.name = "Hope";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+        EnhancedPlayed player = new EnhancedPlayed("Hope", 200, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+
+}
